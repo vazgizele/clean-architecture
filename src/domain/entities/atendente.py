867bab08882ab61357955dc241a22747896694a5
Gizele Vaz Albino da Costa <gizele.costa@transfero.com>
2024-03-27 20:43:21 -0300

    tests for nome

--- src/domain/entities/atendente.py
+++ src/domain/entities/atendente.py
@@ -1,8 +1,7 @@
 from dataclasses import dataclass
 from datetime import date
-
+from src.domain.validators.string_validator import StringValidator
 from src.domain.validators.int_validator import IntValidator
-
 
 @dataclass
 class Atendente:
@@ -15,3 +14,4 @@ class Atendente:
 
     def validade(self):
         IntValidator.validate(value=self.id, field_name="id")
+        StringValidator.validate(value=self.nome, field_name="nome", exact_length=0, min_length=3, max_length=30)
--- tests/unit/entities/test_atendente.py
+++ tests/unit/entities/test_atendente.py
@@ -1,12 +1,9 @@
+from src.domain.entities.atendente import Atendente
 from datetime import date
 import pytest
-
-from src.domain.entities.atendente import Atendente
-
-
 class TestAtendente:
     #id: int
-    def test_id_integer(self):
+    def test_id_not_is_integer(self):
         #Arrange
         atendente = Atendente("abc", "gizele", "18/01/1993", "21990724754", "21990724754", "gizele.costa@transfero.com")
 
@@ -17,7 +14,7 @@ class TestAtendente:
         #Assert
         assert str(error.value) == f'id must be a integer. [value={atendente.id}]'
 
-    def test_id_positive(self):
+    def test_id_not_is_positive(self):
         # Arrange
         atendente = Atendente(-1, "gizele", "18/01/1993", "21990724754", "21990724754", "gizele.costa@transfero.com")
 
@@ -27,3 +24,47 @@ class TestAtendente:
             
         # Assert
         assert str(error.value) == f'id must be a positive integer. [value={atendente.id}]'
+
+    def test_name_is_not_string(self):
+        #Arrange
+        atendente = Atendente(1, 123, "18/01/1993", "21990724754", "21990724754", "gizele.costa@transfero.com")
+
+        # Act
+        with pytest.raises(Exception) as error:
+            atendente.validade()
+            
+        # Assert
+        assert str(error.value) == f"nome must be a string. [value={atendente.nome}]"
+
+    def test_name_is_empty(self):
+        #Arrange
+        atendente = Atendente(1, "", "18/01/1993", "21990724754", "21990724754", "gizele.costa@transfero.com")
+
+        # Act
+        with pytest.raises(Exception) as error:
+            atendente.validade()
+            
+        # Assert
+        assert str(error.value) == f"nome must not be empty."
+
+    def test_name_less_3(self):
+        #Arrange
+        atendente = Atendente(1, "ab", "18/01/1993", "21990724754", "21990724754", "gizele.costa@transfero.com")
+
+        # Act
+        with pytest.raises(Exception) as error:
+            atendente.validade()
+            
+        # Assert
+        assert str(error.value) == f'nome minimum size is - 3'
+
+    def test_name_great_20(self):
+        #Arrange
+        atendente = Atendente(1, "gizele"*6, "18/01/1993", "21990724754", "21990724754", "gizele.costa@transfero.com")
+
+        # Act
+        with pytest.raises(Exception) as error:
+            atendente.validade()
+            
+        # Assert
+        assert str(error.value) == f'nome maximum size is - 30'
