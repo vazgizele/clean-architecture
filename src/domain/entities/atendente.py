619a16bcf8536cb377f7b043ffac59c697c75931
Gizele Vaz Albino da Costa <gizele.costa@transfero.com>
2024-03-28 07:35:06 -0300

    tests for email

--- src/domain/entities/atendente.py
+++ src/domain/entities/atendente.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import date
+from src.domain.validators.email_validator import EmailValidator
 from src.domain.validators.phone_validator import PhoneValidator
 from src.domain.validators.date_validator import DateValidator
 from src.domain.validators.string_validator import StringValidator
@@ -19,3 +20,4 @@ class Atendente:
         StringValidator.validate(value=self.nome, field_name="nome", exact_length=0, min_length=3, max_length=30)
         DateValidator.validate(value=self.data_nascimento, field_name="data_nascimento")
         PhoneValidator.validate(value=self.telefone1, field_name="telefone1")
+        EmailValidator.validate(value=self.email, field_name="email")
--- tests/unit/entities/test_atendente.py
+++ tests/unit/entities/test_atendente.py
@@ -102,3 +102,36 @@ class TestAtendente:
             
         # Assert
         assert str(error.value) == f'telefone1 must be in format [123456789], [value={atendente.telefone1}]'
+
+    def test_email_is_not_none(self):
+        #Arrange
+        atendente = Atendente(1, "gizele", date.today(), '21990724754', '21990724754', None)
+
+        # Act
+        with pytest.raises(Exception) as error:
+            atendente.validade()
+            
+        # Assert
+        assert str(error.value) == f"invalid E-mail. [value=]"
+    
+    def test_email_is_not_string(self):
+        #Arrange
+        atendente = Atendente(1, "gizele", date.today(), '21990724754', '21990724754',123456789)
+
+        # Act
+        with pytest.raises(Exception) as error:
+            atendente.validade()
+            
+        # Assert
+        assert str(error.value) == f"email must be a string. [value={atendente.email}]"
+
+    def test_email_is_not_format(self):
+        #Arrange
+        atendente = Atendente(1, "gizele", date.today(), '21990724754', '21990724754',"gizele.costa@.com")
+
+        # Act
+        with pytest.raises(Exception) as error:
+            atendente.validade()
+            
+        # Assert
+        assert str(error.value) == f"email must be in format [User@Domain.com.br[br=Optional], [value={atendente.email}]"
