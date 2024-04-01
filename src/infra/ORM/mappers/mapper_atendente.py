1860ae456949670639c7ee130492f7718ad4df54
Gizele Vaz Albino da Costa <gizele.costa@transfero.com>
2024-04-01 17:44:00 -0300

    adjustments schema

--- src/infra/ORM/mappers/mapper_atendente.py
+++ src/infra/ORM/mappers/mapper_atendente.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BigInteger, Column, Identity, Table, VARCHAR, String, DATE
+from sqlalchemy import BigInteger, Column, Identity, Table, VARCHAR, String, DATE, Integer
 from sqlalchemy.exc import ArgumentError
 
 from src.domain.entities.atendente import Atendente
@@ -8,13 +8,13 @@ def atentende_mapper(mapper):
     atendente_schema = Table(
         'tb_atendente_gizele',
         mapper.metadata,
-        Column('id', BigInteger, Identity(start=1),primary_key=True, autoincrement=True),
+        Column('id', Integer, Identity(start=1),primary_key=True, autoincrement=True),
         Column('nome', String(50), nullable=False),
         Column('data_nascimento', DATE, nullable=False),
         Column('telefone1', String(11)),
         Column('telefone2', String(11)),
         Column('email', VARCHAR(100), nullable=False),
-        schema="academy"
+        schema="academystage"
     )
     
     try:
